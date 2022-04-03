b84c977b30e56cc75e2bce2b92c302b42abdb2bd
David Kassa <david.kassa@gmail.com>
2022-04-03 15:41:35 -0500

    update dependencies and tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,14 @@ module.exports = {
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    'vue/multi-word-component-names': ['error', {
+      'ignores': ['Content']
+    }]  },
   overrides: [
     {
       files: [
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import Content from "@/components/Content.vue";
 
-describe("HelloWorld.vue", () => {
+describe("Content.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Content, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
